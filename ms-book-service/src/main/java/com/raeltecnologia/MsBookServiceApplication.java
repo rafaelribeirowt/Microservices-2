9bec3827cbe97ddfe4054da8c03447c75b3e88d8
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-11-15 17:42:47 -0300

    Project book-service created

---
+++ ms-book-service/src/main/java/com/raeltecnologia/MsBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raeltecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBookServiceApplication.class, args);
+	}
+
+}
